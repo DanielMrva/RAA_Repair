03c43f5073752f7c6f6eda226ecb94b10813a6d6
Daniel Mrva <mrva.paints@gmail.com>
2024-04-15 20:31:47 -0600

    added front-end variables for mongoDB uri

--- client/src/app/graphql/graphql.module.ts
+++ client/src/app/graphql/graphql.module.ts
@@ -4,6 +4,7 @@ import { ApolloClientOptions, from, InMemoryCache } from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
 import { CommonModule } from '@angular/common';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from '@environments/environment';
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
@@ -16,7 +17,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError.message}, ${networkError.name}, ${networkError.stack}`);
 });
 
-const uri = 'http://localhost:3001/graphql';
+const uri = environment.apiBaseUri;
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUri: 'http://localhost:3001/graphql'
 };
